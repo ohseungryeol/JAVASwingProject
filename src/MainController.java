e40ca708111f3d3407a4ff219a1161b0a33e830a
ggamnol0711 <ggamnol711@gmail.com>
2022-11-08 20:48:46 +0900

    데이터베이스 연동

--- src/MainController.java
+++ src/MainController.java
@@ -3,6 +3,9 @@ public class MainController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DBConnection connection = new DBConnection();
+		System.out.println(connection.isAirport("제주 국제공항"));
+		
 		System.out.println("s");
 	}
 
